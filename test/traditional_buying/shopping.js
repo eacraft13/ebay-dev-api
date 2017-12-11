ded7185421bd69beb14c4a44cf0ec2de7f443dd2
eric <eacraft13@gmail.com>
2017-12-11 15:57:42 -0600

    Update shopping api

--- test/traditional_buying/shopping.js
+++ test/traditional_buying/shopping.js
@@ -1,15 +1,21 @@
 (function () {
     'use strict';
 
-    var finding = require('../../lib/searching/finding')({
+    var ids, shopping;
+
+    ids = [
+        '162528379431',
+        '332187320796',
+        '332029273881'
+    ];
+
+    shopping = require('../../lib/traditional_buying/shopping')({
         appId: 'EricCraf-Finder-PRD-b51ca6568-b6fbb7d2',
     });
 
-    finding.findItemsIneBayStores({
-        storeName: 'Top USA Offers'
-    })
+    shopping.getMultipleItems(ids)
     .then(function (result) {
-        console.log(result);
+        console.log(Object.keys(result[0]));
         process.exit();
     })
     .catch(function (err) {
